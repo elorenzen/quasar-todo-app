fb7ac15b6338a4bf687031f24020b299b3bbd879
Eric <eric@kellyklee.com>
2019-12-11 16:52:18 -0700

    Add task list component with passed props

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,53 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding>
+      <ul>
+          <task
+            v-for="(task, index) in taskList"
+            :task="task"
+            :index="index"
+            v-bind:key="task.title"
+          ></task>
+      </ul>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  components: {
+    'task': require('../components/Task').default
+  },
+  data () {
+    return {
+      taskList: [
+        {
+          title: 'Finish KKX version deployment',
+          dueDate: '12/15/19',
+          dueTime: '9:00'
+        },
+        {
+          title: 'Present CRM to sales team',
+          dueDate: '12/20/19',
+          dueTime: '2:30'
+        },
+        {
+          title: 'Complete design sprint with HOLD',
+          dueDate: '1/6/20',
+          dueTime: '9:00'
+        }
+      ]
+    }
+  },
+  methods: {
+    clearInput () {
+      this.message = ''
+      return this.message
+    },
+    deleteTask (index) {
+      this.taskList.splice(index, 1)
+    }
+  }
 }
 </script>
+<style scoped>
+
+</style>
