f6cc3b45195fa3e037e930426d621aff8c28a864
Eric <eric@kellyklee.com>
2019-12-30 11:40:05 -0700

    Remove array of task data, which has since been refactored as object of objects

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -21,29 +21,6 @@ const state = {
       dueTime: '10:00'
     }
   }
-  /* tasks: [
-    {
-      id: 1,
-      name: 'KKX redeployment changes',
-      completed: false,
-      dueDate: '12/23/19',
-      dueTime: '17:00'
-    },
-    {
-      id: 2,
-      name: 'Follow-up call with DeepWork',
-      completed: false,
-      dueDate: '12/27/19',
-      dueTime: '9:00'
-    },
-    {
-      id: 3,
-      name: "Build 'Marquitta Mobile'",
-      completed: false,
-      dueDate: '2/28/20',
-      dueTime: '10:00'
-    }
-  ] */
 }
 
 const mutations = {
