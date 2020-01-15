a61b68cd943eb97cd90f8401b40b610cdc0479f1
Eric <eric@kellyklee.com>
2020-01-15 05:31:13 -0700

    Add basic form submit method

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -14,53 +14,61 @@
             />
         </q-card-section>
 
-        <q-card-section>
-            <div class="row q-mb-sm">
-                <q-input
-                    outlined
-                    v-model="taskToSubmit.name"
-                    label="Enter your task"
-                    class="col"
-                    :rules="[val => !!val || 'Field is required']"
+        <form @submit.prevent="submitForm">
+            <q-card-section>
+                <div class="row q-mb-sm">
+                    <q-input
+                        outlined
+                        v-model="taskToSubmit.name"
+                        label="Enter your task"
+                        class="col"
+                        :rules="[val => !!val || 'Field is required']"
+                    />
+                </div>
+
+                <div class="row q-mb-sm">
+                    <q-input
+                        outlined
+                        label="Due Date"
+                        v-model="taskToSubmit.dueDate"
+                    >
+                        <template v-slot:append>
+                            <q-icon name="event" class="cursor-pointer">
+                                <q-popup-proxy>
+                                    <q-date v-model="taskToSubmit.dueDate" />
+                                </q-popup-proxy>
+                            </q-icon>
+                        </template>
+                    </q-input>
+                </div>
+
+                <div class="row q-mb-sm">
+                    <q-input
+                        outlined
+                        label="Due Time"
+                        v-model="taskToSubmit.dueTime"
+                    >
+                        <template v-slot:append>
+                            <q-icon name="access_time" class="cursor-pointer">
+                                <q-popup-proxy>
+                                    <q-time v-model="taskToSubmit.dueTime" />
+                                </q-popup-proxy>
+                            </q-icon>
+                        </template>
+                    </q-input>
+                </div>
+            </q-card-section>
+
+            <q-card-actions align="right">
+                <q-btn
+                    flat
+                    label="Save"
+                    color="primary"
+                    v-close-popup
+                    type="submit"
                 />
-            </div>
-
-            <div class="row q-mb-sm">
-                <q-input
-                    outlined
-                    label="Due Date"
-                    v-model="taskToSubmit.dueDate"
-                >
-                    <template v-slot:append>
-                        <q-icon name="event" class="cursor-pointer">
-                            <q-popup-proxy>
-                                <q-date v-model="taskToSubmit.dueDate" />
-                            </q-popup-proxy>
-                        </q-icon>
-                    </template>
-                </q-input>
-            </div>
-
-            <div class="row q-mb-sm">
-                <q-input
-                    outlined
-                    label="Due Time"
-                    v-model="taskToSubmit.dueTime"
-                >
-                    <template v-slot:append>
-                        <q-icon name="access_time" class="cursor-pointer">
-                            <q-popup-proxy>
-                                <q-time v-model="taskToSubmit.dueTime" />
-                            </q-popup-proxy>
-                        </q-icon>
-                    </template>
-                </q-input>
-            </div>
-        </q-card-section>
-
-        <q-card-actions align="right">
-            <q-btn flat label="Save" color="primary" v-close-popup />
-        </q-card-actions>
+            </q-card-actions>
+        </form>
     </q-card>
 </template>
 
@@ -74,6 +82,11 @@ export default {
         dueTime: '',
         completed: false
       }
+    }
+  },
+  methods: {
+    submitForm () {
+      console.log('Form submitted!')
     }
   }
 }
